602cbd5c7812deaa514c4af89babb434477bc410
Athul Aravind K <athularavind002@gmail.com>
2022-05-30 09:16:13 +0530

    unique validator added

--- models/user.js
+++ models/user.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 const schema = mongoose.schema;
+const uniqueValidator = require("mongoose-unique-validator");
+
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+
+userSchema.plugin(uniqueValidator);
 
 const user = mongoose.model("user", userSchema);
 module.exports = user;
